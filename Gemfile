0faef406c6b9521b47a101ed304391a59c34f6be
Matthew Key <matt@mattselkey.com>
2022-01-30 23:17:31 +0100

    fixed comments, added first new post of 2022

--- Gemfile
+++ Gemfile
@@ -1,5 +1,13 @@
 source "https://rubygems.org"
 gem "minima"
 
+# Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
+# and associated library.
+platforms :mingw, :x64_mingw, :mswin, :jruby do
+  gem "tzinfo", ">= 1", "< 3"
+  gem "tzinfo-data"
+end
+
+gem 'wdm', '~> 0.1.1', :install_if => Gem.win_platform?
 
 
--- js/github-comments.js
+++ js/github-comments.js
@@ -1,62 +1,68 @@
 // Original code taken with permission from : https://github.com/dwilliamson/donw.io/blob/master/public/js/github-comments.js
+//
 
-// use of ajax vs getJSON for headers use to get markdown (body vs body_html)
-
-function ShowComments(repo_name, comment_id, page_id)
+async function getComments(repo_name, comment_id, page_id, acc )
 {
-    $.ajax({
-        url: "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id,
-        headers: {Accept: "application/vnd.github.v3.html+json"},
-        dataType: "json",
-        success: function(comments, textStatus, jqXHR) {
+    const url = "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id;
+    const resp = await fetch (url, { headers: {Accept: "application/vnd.github.v3.html+json"}});
+    if (resp.status !== 200) {
+        throw new Error("Response status " + resp.status + " while attempting to fetch comments from: " + url);
+    }
 
-            if (1 == page_id) {
-                // post button 
-                var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
-                $("#gh-comments-list").append("<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>");
+    const comments = await resp.json();
+    
+    // Individual comments
+    for (let i = 0; i < comments.length; i++)
+    {
+        const comment = comments[i];
+        const date = new Date(comment.created_at);
+        acc.push( "<div id='gh-comment'>" + 
+                    "<img src='" + comment.user.avatar_url + "' width='24px'>" + 
+                    "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>" + 
+                    " posted at " + 
+                    "<em>" + date.toUTCString() + "</em>" + 
+                    "<div id='gh-comment-hr'></div>" + 
+                    comment.body_html + 
+                  "</div>" );
+    }
+
+    // Call recursively if there are more pages to display
+
+    const links = resp.headers.get("Link");
+    if (links) {
+        const entries = links.split(",");
+        for (let j=0; j<entries.length; j++)
+        {
+            const entry = entries[j];
+            if (-1 != entry.search('rel="next"'))
+            {
+                acc = await getComments(repo_name, comment_id, page_id+1, acc);
+                break; // recurse on "next" and then stop looking for "next".
             }
-
-            // Individual comments
-            $.each(comments, function(i, comment) {
-
-                var date = new Date(comment.created_at);
-
-                var t = "<div id='gh-comment'>";
-                t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-                t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
-                t += " posted at ";
-                t += "<em>" + date.toUTCString() + "</em>";
-                t += "<div id='gh-comment-hr'></div>";
-                t += comment.body_html;
-                t += "</div>";
-                $("#gh-comments-list").append(t);
-            });
-
-            // Call recursively if there are more pages to display
-            var linksResponse = jqXHR.getResponseHeader("Link");
-            if (linksResponse) {
-                var entries = linksResponse.split(",");
-                for (var j=0; j<entries.length; j++)
-                {
-                    var entry = entries[j];
-                    if ("next" == entry.match(/rel="([^"]*)/)[1])
-                    {
-                        ShowComments(repo_name, comment_id, page_id+1);
-                        break;
-                    }
-                }
-            }
-        },
-        error: function() {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
         }
-    });
+    }
+    return acc;
 }
 
 function DoGithubComments(repo_name, comment_id)
 {
-    $(document).ready(function ()
+    document.addEventListener("DOMContentLoaded", async function() 
     {
-        ShowComments(repo_name, comment_id, 1);
+        try {
+            // post button 
+            const url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
+            const comments = await getComments(repo_name, comment_id, 1, []);
+            const commentsElement = document.getElementById('gh-comments-list');
+
+            // choose one of the next two lines to determine display order (newest or oldest first).
+            const commentsHtml = comments.join('');                // oldest first
+            // const commentsHtml = comments.reverse().join('');   // newest first
+            
+            commentsElement.innerHTML = "<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>" + commentsHtml;
+        } catch (err)
+        {
+            console.log( err.message );
+        }
+        return null;
     });
 }
